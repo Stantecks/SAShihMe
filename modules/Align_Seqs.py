5004e7bb7bccd61cc2205d9f62fe7f18a2505523
izaiahO <67609530+izaiahO@users.noreply.github.com>
2020-10-20 12:13:17 -0700

    Some updates for alignment before I leave JBEI

--- modules/Align_Seqs.py
+++ modules/Align_Seqs.py
@@ -3,8 +3,24 @@ from Bio import SeqIO
 from Bio import pairwise2 
 from Bio.Seq import Seq
 from Quality_control import begin_slide, end_slide, seq_trim
-from Import_files import process_zip, seq_gen
+from Import_files import process_zip, seq_gen, template_seq
+from Bio.pairwise2 import format_alignment
 
 # Starting with importing files (doing one by one)
-my_filenames = process_zip('/SAShihMe/tests/test_zip_from_genewiz.zip')
-my_seq_gen = seq_gen(my_filenames)
+my_filenames = process_zip('/Users/izaiahornelas/Desktop/GitHub/SAShihME/SAShihMe/tests/test_zip_from_genewiz.zip')
+my_gen = seq_gen(my_filenames)
+first_seq = next(my_gen)
+firstseq = first_seq.seq
+print(firstseq)
+
+# my_seq_list = list(my_gen) #turning into list
+# trimmed_seq = [len(seq_trim(10, my_obj.seq)) for my_obj in my_seq_list] # trimming seq 
+
+# Loading template 
+templateseq = template_seq('/Users/izaiahornelas/Desktop/GitHub/SAShihME/SAShihMe/tests/pIO11.gb','genbank')
+
+
+# Aligning seqs
+#alignments = pairwise2.align.globalxx(templateseq,firstseq)
+#for alignment in alignments:
+#     print(format_alignment(*alignment))
